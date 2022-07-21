5857bb014395dd859e728d90214c5c90b88adc3f
Hamees Ahmed Khan <hamees.a@allshorestaffing.com>
2022-07-22 00:37:56 +0500

    Removed unnecessary options from navigation

--- resources/js/frontend/src/examples/Sidenav/SidenavList.vue
+++ resources/js/frontend/src/examples/Sidenav/SidenavList.vue
@@ -61,7 +61,7 @@
           </template>
         </sidenav-collapse>
       </li>
-      <li class="nav-item">
+      <!-- <li class="nav-item">
         <sidenav-collapse url="#" :aria-controls="''" v-bind:collapse="false" collapseRef="tables" navText="Tables">
           <template v-slot:icon>
             <i class="material-icons-round opacity-10 fs-5">table_view</i>
@@ -117,14 +117,14 @@
             <i class="material-icons-round opacity-10 fs-5">assignment</i>
           </template>
         </sidenav-collapse>
-      </li>
+      </li> -->
     </ul>
-    <div class="sidenav-footer position-absolute w-100 bottom-0">
+    <!-- <div class="sidenav-footer position-absolute w-100 bottom-0">
       <div class="mx-3">
         <a class="btn mt-4 w-100" :class="`bg-gradient-${this.$store.state.color}`"
           href="https://www.creative-tim.com/product/vue-material-dashboard-2-pro">Upgrade to pro</a>
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
 <script>
